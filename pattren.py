cb5656e0a5bc26ed36a4064351147e81705fcfc1
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2020-06-11 00:07:39 +0530

    Update pattren.py

--- pattren.py
+++ pattren.py
@@ -1,6 +1,9 @@
-for i in range(6):
-    for x in range(i):
-        print(i,end=" ")
-    print('\n')
-    
+def pattern(a):
+        for i in range(0,a):
+            for x in range(0,i):
+               print('*',end=" ")
+            print('\n')
+a=int(input('Enter the rows value:'))
+pattern(a)    
+
 
